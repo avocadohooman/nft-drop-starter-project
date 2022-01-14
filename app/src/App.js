2d2846348d1594e86b433be6cbd5eaaaa79972f8
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-14 11:48:49 +0200

    web client working

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './CandyMachine';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+
+	const [walletAddress, setWalletAddress] = useState(null);
+
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if (solana && solana.isPhantom) {
+				console.log('Phantom wallet found!', solana.isPhantom);
+				const response = await solana.connect({ onlyIfTrusted: true});
+				console.log(
+					'Connected with public key:',
+					response.publicKey.toString());
+				setWalletAddress(response.publicKey.toString());
+			} else {
+				alert('Solana object not found! Get a Phantom Wallet 👻');
+			}
+		} catch (error) {
+			console.log('error', error);
+		}
+	}
+
+	const connectWallet = async () => {
+		const { solana } = window;
+		try {
+			if (solana) {
+				const response = await solana.connect();
+				console.log('Connected with Public Key:', response.publicKey.toString());
+				setWalletAddress(response.publicKey.toString());			
+			}
+		} catch (error) {
+			console.log('error', error);
+		}
+	}
+
+	/*
+	* We want to render this UI when the user hasn't connected
+	* their wallet to our app yet.
+	*/
+	const renderNotConnectedContainer = () => (
+		<button
+		className="cta-button connect-wallet-button"
+		onClick={connectWallet}
+		>
+			Connect to Wallet
+		</button>
+	);
+
+
+	useEffect(() => {
+		/*
+			Unlike meta mask and the ehtereum object, we need to wait for the window to have
+			fully finished loading the page. before checking for the solana object. 
+			Once this event gets called, we can guarantee that this object is available 
+			if the user has the Phantom Wallet extension installed. d
+		*/
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		  };
+		  window.addEventListener('load', onLoad);
+		  return () => window.removeEventListener('load', onLoad);
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+				<p className="header">🍭 Candy Drop</p>
+				<p className="sub-text">NFT drop machine with fair mint</p>
+				{!walletAddress && renderNotConnectedContainer()}
+				{walletAddress && <CandyMachine walletAddress={window.solana} />}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
